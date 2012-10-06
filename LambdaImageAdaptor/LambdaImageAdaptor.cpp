f12f11d27e7c772cfe172ae43134eac709ac07e2
David Doria <daviddoria@gmail.com>
2012-10-06 08:32:01 -0400

    Add LambdaImageAdaptor

---
+++ LambdaImageAdaptor/LambdaImageAdaptor.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include "itkImage.h"
+#include "itkImageRegionIterator.h"
+
+#include "itkLambdaImageAdaptor.h"
+
+template <typename TImage>
+static void CreateImage(TImage* const image);
+
+int main(int argc, char** argv)
+{
+  // Create an image
+  const unsigned int  Dimension = 2;
+  typedef int                                  PixelType;
+  typedef itk::Image< PixelType, Dimension >   ImageType;
+
+  ImageType::Pointer image = ImageType::New();
+
+  CreateImage(image.GetPointer());
+
+  // Create a lambda
+  auto myLambda = [](const ImageType::PixelType& pixel) {
+      return pixel + 3;
+   };
+
+  // Create the adaptor
+  typedef itk::LambdaImageAdaptor<  ImageType,
+    decltype(myLambda) > LambdaAdaptorType;
+
+  LambdaAdaptorType::Pointer adaptor = LambdaAdaptorType::New();
+  adaptor->SetImage(image);
+  adaptor->SetLambda(myLambda);
+
+  // Output the result of applying the lambda to every pixel
+  itk::ImageRegionIterator<LambdaAdaptorType>
+  outputIterator(adaptor,adaptor->GetLargestPossibleRegion());
+
+  while(!outputIterator.IsAtEnd())
+  {
+    std::cout << outputIterator.Get();
+    ++outputIterator;
+  }
+  std::cout << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+template <typename TImage>
+void CreateImage(TImage* const image)
+{
+  typename TImage::IndexType corner = {{0,0}};
+  typename TImage::SizeType size = {{2,2}};
+  typename TImage::RegionType region(corner,size);
+  image->SetRegions(region);
+  image->Allocate();
+  unsigned int counter = 0;
+
+  itk::ImageRegionIterator<TImage>
+      imageIterator(image,image->GetLargestPossibleRegion());
+
+  while(!imageIterator.IsAtEnd())
+  {
+    std::cout << counter;
+    imageIterator.Set(counter);
+    counter++;
+    ++imageIterator;
+  }
+  std::cout << std::endl;
+}
---
+++ LambdaImageAdaptor/itkLambdaImageAdaptor.h
@@ -0,0 +1,106 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkLambdaImageAdaptor_h
+#define __itkLambdaImageAdaptor_h
+
+#include "itkImageAdaptor.h"
+#include "vnl/vnl_math.h"
+
+#include <functional>
+
+namespace itk
+{
+namespace Accessor
+{
+/** \class LambdaPixelAccessor
+ * \brief Give access to an arbitrary function of a value
+ *
+ * \ingroup ImageAdaptors
+ * \ingroup ITKImageAdaptors
+ */
+template< class TInternalType, typename TLambda >
+class ITK_EXPORT LambdaPixelAccessor
+{
+public:
+  /** Required by ImageAdaptor. */
+  typedef TInternalType InternalType;
+  typedef TInternalType ExternalType;
+
+  std::function<InternalType(InternalType)> m_Lambda;
+
+  void SetLambda(TLambda lambda)
+  {
+    m_Lambda = lambda;
+  }
+
+  inline TInternalType Get(const TInternalType & input) const
+  { return m_Lambda( input ); }
+
+};
+} // end namespace Accessor
+
+/** \class LambdaImageAdaptor
+ * \brief Presents an image as being composed of an arbitrary function
+ * of its pixels
+ *
+ * \ingroup ImageAdaptors
+ * \ingroup ITKImageAdaptors
+ */
+template< class TImage, typename TLambda >
+class ITK_EXPORT LambdaImageAdaptor:public
+  ImageAdaptor< TImage,
+                Accessor::LambdaPixelAccessor<
+                  typename TImage::PixelType,
+                  TLambda >   >
+{
+public:
+  /** Standard class typedefs. */
+  typedef LambdaImageAdaptor Self;
+  typedef ImageAdaptor< TImage, Accessor::LambdaPixelAccessor<
+                          typename TImage::PixelType,
+                          TLambda > >  Superclass;
+
+  typedef SmartPointer< Self >       Pointer;
+  typedef SmartPointer< const Self > ConstPointer;
+
+  typedef Accessor::LambdaPixelAccessor<
+    typename TImage::PixelType,
+    TLambda >  AccessorType;
+
+  void SetLambda(TLambda lambda)
+  {
+    AccessorType accessor;
+    accessor.SetLambda(lambda);
+    Superclass::SetPixelAccessor(accessor);
+  }
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(LambdaImageAdaptor, ImageAdaptor);
+protected:
+  LambdaImageAdaptor() {}
+  virtual ~LambdaImageAdaptor() {}
+private:
+  LambdaImageAdaptor(const Self &); //purposely not implemented
+  void operator=(const Self &);  //purposely not implemented
+};
+} // end namespace itk
+
+#endif
